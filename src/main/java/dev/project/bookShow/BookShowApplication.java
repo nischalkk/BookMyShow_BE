3cef1f7fb9f221cd6af1ff07e83092ff9be676a5
Nischal G <49017422+nischalkk@users.noreply.github.com>
2025-01-04 19:37:10 +0530

    Created SpringBoot template......

---
+++ src/main/java/dev/project/bookShow/BookShowApplication.java
@@ -0,0 +1,13 @@
+package dev.project.bookShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/project/bookShow/BookShowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.project.bookShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
